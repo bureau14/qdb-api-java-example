c4fdfb33d3b3e90bc57450cff32507e76e08be92
Leon Mergen <leon@solatis.com>
2018-02-04 14:56:16 +0100

    Update example according to new API

--- src/main/java/QdbExample.java
+++ src/main/java/QdbExample.java
@@ -1,115 +1,178 @@
-import net.quasardb.qdb.*;
+import net.quasardb.qdb.Session;
+import net.quasardb.qdb.ts.Table;
+import net.quasardb.qdb.ts.Writer;
+import net.quasardb.qdb.ts.Reader;
+import net.quasardb.qdb.ts.Column;
+import net.quasardb.qdb.ts.Value;
+import net.quasardb.qdb.ts.Row;
+import net.quasardb.qdb.ts.TimeRange;
+import net.quasardb.qdb.ts.Timespec;
+
+import net.quasardb.qdb.exception.ConnectionRefusedException;
+
+import java.util.UUID;
+import java.util.Random;
+
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 /**
  * An example of a Java program using Quasardb
  */
 public class QdbExample {
-  final static String clusterUri = "qdb://127.0.0.1:2836";
 
-  public static void main(String[] args) {
-    QdbCluster db = connectToCluster();
-    playWithInteger(db);
-    playWithBlob(db);
-    playWithDeque(db);
-    playWithTag(db);
-    removeEntries(db);
-  }
+    // Use a time range from -1h until +1h
+    static final TimeRange[] DEFAULT_RANGE = {
+        new TimeRange(Timespec.now(),
+                      Timespec.now().plusSeconds(3600))
+    };
 
-  static QdbCluster connectToCluster() {
-    try {
-      System.out.println("Connecting to " + clusterUri);
-      return new QdbCluster(clusterUri);
-    } catch (QdbConnectionRefusedException ex) {
-      System.err.println("Failed to connect to " + clusterUri +
-                         ", make sure server is running!");
-      System.exit(1);
-      return null;
+    public static void main(String[] args) throws IOException {
+        Session session = connectToCluster("qdb://127.0.0.1:2836");
+
+        compareWriters(session);
     }
-  }
 
-  static void playWithInteger(QdbCluster db) {
-    QdbInteger entry = db.integer("example.integer");
-
-    System.out.println("Create an integer entry");
-    entry.put(12);
-
-    System.out.println("Replace its value");
-    entry.update(34);
-
-    System.out.println("Increment");
-    entry.add(8);
-
-    System.out.print("Read value: ");
-    System.out.println(entry.get());
-  }
-
-  static void playWithBlob(QdbCluster db) {
-    QdbBlob entry = db.blob("example.blob");
-
-    ByteBuffer sample = createByteBuffer("Hello World!");
-
-    System.out.println("Create a blob");
-    entry.put(sample);
-
-    System.out.print("Read blob: ");
-    QdbBuffer content = entry.get();
-    System.out.println(content.toByteBuffer().asCharBuffer());
-    content.close();
-  }
-
-  static void playWithDeque(QdbCluster db) {
-    QdbDeque entry = db.deque("example.deque");
-
-    ByteBuffer sample1 = createByteBuffer("sample1");
-    ByteBuffer sample2 = createByteBuffer("sample2");
-
-    System.out.println("Push to the back of the deque");
-    entry.pushBack(sample1);
-    entry.pushBack(sample2);
-
-    System.out.println("Read deque in from the front: ");
-    QdbBuffer content = entry.popBack();
-    while (content != null) {
-      System.out.println(content.toByteBuffer().asCharBuffer());
-      content.close();
-      content = entry.popBack();
+    static Session connectToCluster(String uri) {
+        try {
+            return Session.connect(uri);
+        } catch (ConnectionRefusedException ex) {
+            System.err.println("Failed to connect to " + uri +
+                               ", ma ke sure server is running!");
+            System.exit(1);
+            return null;
+        }
     }
-  }
 
-  static void playWithTag(QdbCluster db) {
-    QdbTag tag = db.tag("example.tag");
+    /**
+     * This function illustrates that QuasarDB has various buffering strategies
+     * for writing data into QuasarDB, and the different semantics that come with
+     * each of them.
+     *
+     * Which one you should use depends upon your use case. When in doubt, however,
+     * we recommend using an autoFlushWriter.
+     */
+    static void compareWriters(Session session) throws IOException {
+        System.out.println("== Creating tables ==");
+        Table t1 = createTable(session);
+        Table t2 = createTable(session);
+        Table t3 = createTable(session);
+        System.out.println("== Done! ==");
+        System.out.println();
 
-    System.out.println("Tag the integer from the tag handle");
-    db.blob("example.integer").attachTag(tag);
-    db.blob("example.blob").attachTag(tag);
-    db.deque("example.deque").attachTag(tag);
+        /**
+         * Creating a regular Writer using `Table.writer` will initialise a writer
+         * that does not perform any automatic flushing at all.
+         */
+        Writer w1 = Table.writer(session, t1);
 
-    System.out.println("Enumerate tagged entries:");
-    for (QdbEntry entry : tag.entries()) {
-      System.out.println(entry.alias());
-      if (entry instanceof QdbInteger)
-        System.out.println("Value is: " + ((QdbInteger)entry).get());
+        System.out.println("== Writing with regular writer ==");
+        writeTable(session, w1, 75000);
+
+        /**
+         * As you can see directly after writing, none of the data has been written.
+         * At this point all data is still in cache.
+         */
+        readTable(session, Table.reader(session, t1, DEFAULT_RANGE));
+
+        /**
+         * Only after we explicitly flush the writer, the data becomes visible.
+         */
+        w1.flush();
+        readTable(session, Table.reader(session, t1, DEFAULT_RANGE));
+
+        System.out.println("== Done! ==");
+        System.out.println();
+
+        /**
+         * Creating an AutoFlushWriter using `Table.autoFlushWriter` will initialise a
+         * writer that periodically flushes the internal buffer with a default buffer size
+         * of 50000 rows.
+         */
+        Writer w2 = Table.autoFlushWriter(session, t2);
+
+        System.out.println("== Writing with auto flush writer ==");
+        writeTable(session, w2, 75000);
+
+        /**
+         * As you can see directly after 75000 rows, only the first chunk of 50000 rows
+         * have been written.
+         */
+        readTable(session, Table.reader(session, t2, DEFAULT_RANGE));
+
+        /**
+         * After we either explicitly flush the writer, or write another 25000 rows, the
+         * data becomes visible.
+         */
+        w2.flush();
+        readTable(session, Table.reader(session, t2, DEFAULT_RANGE));
+
+        System.out.println("== Done! ==");
+        System.out.println();
+
+        /**
+         * Creating an AutoFlushWriter using `Table.autoFlushWriter` with a buffer size
+         * of 1 row will initialise a writer that flushes the internal buffer after every
+         * write operation.
+         *
+         * A buffer size of 1 row should typically not be used since performance will suffer,
+         * but it does illustrate the configurability of the AutoFlushWriter.
+         */
+        Writer w3 = Table.autoFlushWriter(session, t3, 1);
+
+        System.out.println("== Writing with auto flush writer with buffer length 1 ==");
+        writeTable(session, w3, 75000);
+        readTable(session, Table.reader(session, t3, DEFAULT_RANGE));
+
+
+        /**
+         * And in this specific case, flushing is unnecessary so we are done here.
+         */
+        System.out.println("== Done! ==");
+        System.out.println();
     }
-  }
 
-  static void removeEntries(QdbCluster db) {
-    System.out.println("Delete the integer");
-    db.integer("example.integer").remove();
+    static void writeTable(Session s, Writer w, long count) throws IOException {
+        System.out.println("Writing " + count + " rows to table...!");
+        for (long i = 0; i < count; ++i) {
+            w.append(randomRow());
+        }
+    }
 
-    System.out.println("Delete the blob");
-    db.blob("example.blob").remove();
+    static void readTable(Session s, Reader w) {
+        System.out.println("Reading table...!");
+    }
 
-    System.out.println("Delete the deque");
-    db.deque("example.deque").remove();
+    static Table createTable(Session session) {
+        // We generate a random name here, but you're free to use
+        // anything you want.
+        String name = UUID.randomUUID().toString();
 
-    System.out.println("Delete the tag");
-    db.tag("example.tag").remove();
-  }
+        Column[] columns = {
+            new Column.Double("double_val"),
+            new Column.Int64("int_val"),
+            new Column.Timestamp("ts_val")
+        };
 
-  static ByteBuffer createByteBuffer(String content) {
-    ByteBuffer bb = ByteBuffer.allocateDirect(content.length() * 2);
-    bb.asCharBuffer().put(content);
-    return bb;
-  }
+        System.out.println("Creating table '" + name + "'");
+
+        return Table.create(session, name, columns);
+    }
+
+    /**
+     * Utility function that generates a random row that matches the table layout
+     * created in createTable.
+     */
+    static Row randomRow() {
+        Random r = new Random();
+
+        Value[] values = {
+            Value.createDouble(r.nextDouble()), // double_val
+            Value.createInt64(r.nextLong()), // int_val
+            Value.createTimestamp(Timespec.now()) // ts_val
+
+        };
+
+        return new Row(Timespec.now(), values);
+    }
 }
